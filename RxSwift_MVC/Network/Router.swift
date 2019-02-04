d7cf2c653edf36f6d04b5f14b8c9c132b36c76d5
이광용 <rhkdrmfh@gmail.com>
2019-02-04 17:15:24 -0500

    Update Router with Rx

---
+++ RxSwift_MVC/Network/Router.swift
@@ -0,0 +1,83 @@
+//
+//  APIRouter.swift
+//  RxSwift_MVC
+//
+//  Created by 이광용 on 04/02/2019.
+//  Copyright © 2019 GwangYongLee. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+typealias RouterCompletion = (_ data: Data?, _ response: URLResponse?, _ error: Error?) -> Void
+protocol RouterProtocol {
+    func request(from route: EndPoint) throws -> URLRequest
+}
+
+class Router: RouterProtocol {
+    func request(from route: EndPoint) throws -> URLRequest {
+        var urlRequest = URLRequest(url: route.baseURL.appendingPathComponent(route.path),
+                                    cachePolicy: .reloadIgnoringLocalAndRemoteCacheData,
+                                    timeoutInterval: 10.0)
+        urlRequest.httpMethod = route.httpMethod.rawValue
+        do {
+            switch route.task {
+            case .request(let bodyParameters, let urlParameters, let headers):
+                try urlRequest.congifure(body: bodyParameters, url: urlParameters, headers: headers)
+            case .download, .upload:
+                throw NetworkError.unknown
+            }
+        } catch {
+            throw error
+        }
+        return urlRequest
+    }
+}
+
+extension Reactive where Base: Router {
+    func response(about request: URLRequest) -> Observable<(response: HTTPURLResponse, data: Data)> {
+        return Observable.create { observer in
+            let task = URLSession.shared.dataTask(with: request) { data, response, error in
+                guard let response = response, let data = data else {
+                    observer.on(.error(error ?? NetworkError.unknown))
+                    return
+                }
+                
+                guard let httpResponse = response as? HTTPURLResponse else {
+                    observer.on(.error(error ?? NetworkError.none(response: response)))
+                    return
+                }
+                
+                observer.on(.next((httpResponse, data)))
+                observer.on(.completed)
+            }
+            
+            task.resume()
+            
+            return Disposables.create {
+                task.cancel()
+            }
+        }
+    }
+    
+    func data(about request: URLRequest) -> Observable<Data> {
+        return self.response(about: request).map{
+            switch $0.response.statusCode {
+            case 200..<300:
+                return $0.data
+            default:
+                throw NetworkError.requestFailed(response: $0.response, data: $0.data)
+            }
+        }
+    }
+    
+    func json(about request: URLRequest, options: JSONSerialization.ReadingOptions = []) -> Observable<Any> {
+        return self.data(about: request).map {
+            do {
+                return try JSONSerialization.jsonObject(with: $0, options: options)
+            } catch let error {
+                throw NetworkError.deserialization(error: error)
+            }
+        }
+    }
+}
